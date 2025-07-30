1be228f7d178fdc28629af3e7e5b9ee91b4eba6b
Praveenarumugam07 <96639707+Praveenarumugam07@users.noreply.github.com>
2025-07-30 15:35:26 +0530

    Create otel_config.py

---
+++ app/otel_config.py
@@ -0,0 +1,16 @@
+from opentelemetry import trace
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+
+def init_tracer(service_name: str):
+    trace.set_tracer_provider(
+        TracerProvider(
+            resource=Resource.create({SERVICE_NAME: service_name})
+        )
+    )
+
+    otlp_exporter = OTLPSpanExporter(endpoint="http://34.47.150.135:4318/v1/traces")
+    span_processor = BatchSpanProcessor(otlp_exporter)
+    trace.get_tracer_provider().add_span_processor(span_processor)
