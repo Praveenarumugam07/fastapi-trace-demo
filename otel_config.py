b0a79496d80dea41c785a7fd3e142ffd8231d377
Praveenarumugam07 <96639707+Praveenarumugam07@users.noreply.github.com>
2025-07-30 16:51:12 +0530

    Update otel_config.py

--- otel_config.py
+++ otel_config.py
@@ -1,12 +1,12 @@
 from opentelemetry import trace
-from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 def configure_tracer():
     trace.set_tracer_provider(TracerProvider())
     jaeger_exporter = JaegerExporter(
-        agent_host_name="34.9.8.45",
+        agent_host_name="34.9.8.45",  # replace this
         agent_port=6831,
     )
     span_processor = BatchSpanProcessor(jaeger_exporter)
